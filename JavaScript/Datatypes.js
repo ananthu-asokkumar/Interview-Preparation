eb0d23295ee0328b12c98f74bbd5110022f75fa6
ananthu-asokkumar <ananthu_fw10_048@masai.school>
2021-11-19 10:43:47 +0530

    Datatypes

---
+++ JavaScript/Datatypes.js
@@ -0,0 +1,16 @@
+/**
+ * Primitive Datatypes:In javascript primitive is a data that is not an object and has no methods .
+ * There are 7 primitive data types: string, number, bigint, boolean, undefined, symbol, and null.
+ *
+ *
+ * All primitives are immutable, i.e., they cannot be altered. It is important not to confuse a primitive itself with a
+ * variable assigned a primitive value. The variable may be reassigned a new value, but the existing value can not be 
+ * changed in the ways that objects, arrays, and functions can be altered.
+ * 
+ * Abstract datatypes
+ * 1) objects
+ * 2)arrays
+ * 3)Sets
+ * 4)Maps
+ * 
+ */
