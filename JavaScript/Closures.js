44256bf37bb6d449aad33b82cbe6e9f0ae3cc5de
ananthu-asokkumar <ananthu_fw10_048@masai.school>
2021-11-24 08:00:05 +0530

    closures

--- JavaScript/Closures.js
+++ JavaScript/Closures.js
@@ -11,3 +11,19 @@ function test(a=8,b=7) {
 }
 let d = test(6,8);
 d();
+
+/**
+ * ********CLOSURES****
+ * A Closure is a combination of function bundled together with its lexical environment.
+ * 
+ *  In other words, a closure gives you access to an outer function’s scope from an inner function. In JavaScript,
+ *  closures are created every time a function is created, at function creation time.
+ * 
+ * 
+ * 
+ * 
+ * here when function s is called from outside d() the function s remembered the value 3 
+ * which is in its lexical environment due to closures
+ */
+
+
