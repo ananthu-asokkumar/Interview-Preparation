12c8d6fc3f7b2f357b708c0ea51b113261e73251
ananthu-asokkumar <ananthu_fw10_048@masai.school>
2021-12-11 23:35:55 +0530

    rest and spread operators

---
+++ JavaScript/Rest&Spread.js
@@ -0,0 +1,21 @@
+// rest parameter should be at last only
+const sum = (...args) => { //we can use rest to take n no of arguments to a fn
+    
+    // console.log(arguments); arguments object doesnot work with arrow fn
+    console.log(args);
+}
+
+sum(1, 2, 3, 4);
+
+function sum1() {
+    console.log(arguments); // ES5
+}
+
+sum1(1, 2, 3, 4);
+
+/**
+ * to find max value of an array we can use spread operator
+ */
+
+let arr = [1, 23, 45, 6, 4, 343, 5];
+console.log(Math.max(...arr));
---
+++ JavaScript/undefined&null.js
@@ -0,0 +1,12 @@
+
+
+
+console.log(undefined === null);
+
+
+var a;
+console.log(typeof a);
+ 
+var x = 10;
+var x = 4;
+console.log(x);
