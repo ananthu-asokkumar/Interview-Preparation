fba956891696e022a6e728b91b207692d951dd2c
ananthu-asokkumar <ananthu_fw10_048@masai.school>
2021-12-13 21:11:27 +0530

    object literals

---
+++ JavaScript/generators.js
@@ -0,0 +1,23 @@
+
+
+/**
+ * Generators can help you to pause and resume the code
+ * 
+ * 
+ * Normally when you write a function ,it returns a single value/
+ * You can think of generators as a kind of function which can return multiple
+ * values in phases 
+ * 
+ * The function* is the keyword used to define a generator function
+ * 
+ * yield is an operator which pauses the generator.
+ * yield also helps to receive input and send outputs.
+ * 
+ * 
+ */
+
+//ex:1
+
+const genfunction = function* () {
+    
+}
